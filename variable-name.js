0dc69810438b3b2769e5c89dcb2445bc7a051c36
evo264714 <mh264714@gmail.com>
2023-01-26 16:45:05 +0600

    var

---
+++ variable-name.js
@@ -0,0 +1,33 @@
+var price = 29;
+
+// *1- Variable name can not be any keywords
+
+var false = 96;
+var return = true;
+
+// *2. Variable name has to be in one word. No space
+
+var my home address ="Dhaka";
+
+// *3. Variable name can not have quotations-
+
+var "name" = 'Tom Cruise';
+
+// *4. Variable name can not starts with a number but can ends with a number
+
+var 99club = 1964;
+var club25 = 2025;
+
+// *5. How to use long names 
+// *Can not use dash
+
+var user-name = "evo";
+
+// * Can use _
+
+var user_name = "evo";
+
+// *Camel Case
+var user_home_address = "Dhaka";
+var userHomeAddress = "Dhaka";
+
--- variable.js
+++ variable.js
@@ -1,4 +1,17 @@
 //* vary + able = variable
+
+// *Numeric
 var price = 13;
 var age = 24;
 var temperature = 38;
+
+// *String
+var person = "Evo" ;
+var location = "Bandarban" ;
+var name = 'Mehedi Hasan' ;
+
+// *Boolean
+
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
