f03a44f646f57586f9a914fc85d83304b95e7da8
evo264714 <mh264714@gmail.com>
2023-01-26 16:01:34 +0600

    first

---
+++ math.js
@@ -0,0 +1,15 @@
+//      !1- Number ==> 55.898, -32 
+//      !2- Integer: 2, 5
+//      !3- Float: Decimal: 2.5, 13.67
+//      !4- Operation: AudioDestinationNode,       Substraction, Multiplication, Division, Remainder
+//      !5- Percentage
+//      !6- Average
+//      !7- Comparison: >, <, ==, !==, >=, <=
+//      !8- Oikik 
+//      !9- 45,25,68,68,129: max,min, average, sum
+//      !10- Series
+
+//      *EXTRA-
+
+//       !1- Prime
+//       !2- Permutation
